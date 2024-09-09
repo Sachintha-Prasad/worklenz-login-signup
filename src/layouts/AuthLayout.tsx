6ac2da7c41ac8144f4474110836f2aa6519dac7c
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-09 15:33:23 +0530

    imp: implement a global layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,11 +1,18 @@
+import { Col, Row } from "antd"
 import React from "react"
 import { Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
     return (
-        <div>
-            <Outlet />
-        </div>
+        <Row>
+            <Col
+                style={{ marginTop: 120 }}
+                xs={{ span: 20, offset: 2, flex: "100%" }}
+                sm={{ span: 12, offset: 6, flex: "100%" }}
+            >
+                <Outlet />
+            </Col>
+        </Row>
     )
 }
 
