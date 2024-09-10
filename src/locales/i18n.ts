6a9bd36225d281fca11a06b5e2c62a6296541626
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-10 21:17:03 +0530

    feat: i18n internationalization implemented

---
+++ src/locales/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
+import enLng from "./en/en.json"
+import siLng from "./si/si.json"
+
+const resources = {
+    en: {
+        translation: enLng
+    },
+    si: {
+        translation: siLng
+    }
+}
+
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: "si",
+        fallbackLng: "en",
+        interpolation: {
+            escapeValue: false
+        }
+    })
+
+export default i18n
