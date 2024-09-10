dea672fa243a42bcadefd2b465b9d178c317ce57
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-10 10:51:08 +0530

    update: page header aded

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,11 +1,12 @@
 import { Button, Flex } from "antd"
 import React from "react"
 import { Link } from "react-router-dom"
+import PageHeader from "../components/PageHeader"
 
 const UserPage = () => {
     return (
         <Flex vertical align="center" gap={8}>
-            <h1>Wellcome to user page!</h1>
+            <PageHeader description="Wellcome to user page" />
             <Link to="/auth/login">
                 <Button
                     block
