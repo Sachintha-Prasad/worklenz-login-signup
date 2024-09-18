6d9a822946b65a3e592439b860204ee6cac521ff
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-18 14:49:05 +0530

    chore: move i18n.ts file to features/locales directory

--- src/features/locales/i18n.ts
+++ src/features/locales/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import LanguageDetector from "i18next-browser-languagedetector"
-import enLng from "./en/en.json"
-import siLng from "./si/si.json"
+import enLng from "../../lib/locales/en/en.json"
+import siLng from "../../lib/locales/si/si.json"
 
 const resources = {
     en: {
@@ -17,7 +17,6 @@ i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources,
-        lng: "en",
         fallbackLng: "en",
         interpolation: {
             escapeValue: false
