33b039400f036444e78c0d9f4cc66d18c8eaa32a
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-18 14:43:42 +0530

    refactor(index): wrap the main app by the redux provide

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-import "./locales/i18n"
+import "./features/locales/i18n"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
 )
 
 reportWebVitals()
