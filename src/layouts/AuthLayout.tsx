63d5b45a71a47268bc077174a335ab22e0cc8d16
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-23 20:49:52 +0530

    chore(layout): clean up Auth layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,19 +1,17 @@
-import { Col, Layout, Row } from "antd"
+import { Col, Layout } from "antd"
 import React from "react"
 import { Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
     return (
         <Layout style={{ minHeight: "100vh" }}>
-            <Row>
-                <Col
-                    style={{ marginTop: 120 }}
-                    xs={{ span: 20, offset: 2, flex: "100%" }}
-                    sm={{ span: 12, offset: 6, flex: "100%" }}
-                >
-                    <Outlet />
-                </Col>
-            </Row>
+            <Col
+                style={{ marginTop: 120 }}
+                xs={{ span: 20, offset: 2, flex: "100%" }}
+                sm={{ span: 12, offset: 6, flex: "100%" }}
+            >
+                <Outlet />
+            </Col>
         </Layout>
     )
 }
