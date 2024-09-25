2c4cf12d8185f97bfb1dd1c16c7122497123099c
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-25 17:27:37 +0530

    feat(project page): add project page

---
+++ src/pages/projects/ProjectPage.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ProjectPage = () => {
+    return (
+        <div style={{ marginBlock: 120, minHeight: "75vh" }}>ProjectPage</div>
+    )
+}
+
+export default ProjectPage
