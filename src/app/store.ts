37bc06dc5acf12ee20bb118d547559775b2e51a2
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-25 17:20:06 +0530

    feat(store): update store with notification reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import themeReducer from '../features/theme/themeSlice'
 import localesReducer from '../features/i18n/localesSlice'
 import projectReducer from '../features/projects/projectSlice'
+import notificationReducer from '../features/notification/notificationSlice'
 
 export const store = configureStore({
     reducer: {
         themeReducer: themeReducer,
         localesReducer: localesReducer,
         projectReducer: projectReducer,
+        notificationReducer: notificationReducer,
     },
 })
 
