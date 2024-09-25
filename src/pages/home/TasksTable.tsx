fe051fbe54158d066368161adf6316db43824b65
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-25 17:22:26 +0530

    refactor(task table): update task table with, add a calender and calender toggle with segment, and implement the spin animantion to the sync icon

--- src/pages/home/TasksTable.tsx
+++ src/pages/home/TasksTable.tsx
@@ -1,4 +1,4 @@
-import { SyncOutlined } from "@ant-design/icons"
+import { SyncOutlined } from '@ant-design/icons'
 import {
     Button,
     Card,
@@ -8,11 +8,33 @@ import {
     Segmented,
     Select,
     Table,
-    Typography
-} from "antd"
-import React from "react"
+    Tabs,
+    Typography,
+    Calendar,
+} from 'antd'
+import React, { useState } from 'react'
 
 const TasksTable = () => {
+    const [isCalenderOpen, setIsCalenderOpen] = useState(false)
+    const [isSpin, setIsSpin] = useState(false)
+
+    // function for handle spin animation
+    const handleSpinAnimation = () => {
+        setIsSpin(true)
+        setTimeout(() => setIsSpin(false), 500)
+    }
+
+    // function for handle segmaent change and render the calender
+    const handleSegmentChange = (value: string) => {
+        if (value === 'Calendar') {
+            setIsCalenderOpen(true)
+            handleSpinAnimation()
+        } else {
+            setIsCalenderOpen(false)
+            handleSpinAnimation()
+        }
+    }
+
     return (
         <Card
             title={
@@ -24,28 +46,65 @@ const TasksTable = () => {
                         defaultValue="assigned to me"
                         options={[
                             {
-                                value: "assigned to me",
-                                label: "assigned to me"
+                                value: 'assigned to me',
+                                label: 'assigned to me',
                             },
-                            { value: "assigned by me", label: "assigned by me" }
+                            {
+                                value: 'assigned by me',
+                                label: 'assigned by me',
+                            },
                         ]}
                     />
                 </Flex>
             }
             extra={
                 <Flex gap={8} align="center">
-                    <Button shape="circle" icon={<SyncOutlined />} />
+                    <Button
+                        shape="circle"
+                        icon={<SyncOutlined spin={isSpin} />}
+                        onClick={() => handleSpinAnimation()}
+                    />
                     <Segmented<string>
-                        options={["List", "Calendar"]}
+                        options={['List', 'Calendar']}
                         defaultValue="List"
+                        onChange={(value) => handleSegmentChange(value)}
                     />
                 </Flex>
             }
-            style={{ width: "100%", maxWidth: 1200 }}
+            style={{ width: '100%' }}
         >
-            <Input
-                placeholder="+ Add Task"
-                style={{ width: "100%", maxWidth: 300 }}
+            {isCalenderOpen && <Calendar />}
+
+            <Tabs
+                type="card"
+                items={[
+                    {
+                        key: 'all',
+                        label: `All (0)`,
+                        children: (
+                            <Input
+                                placeholder="+ Add Task"
+                                style={{ width: '100%', maxWidth: 300 }}
+                            />
+                        ),
+                    },
+                    {
+                        key: 'today',
+                        label: `Today (0)`,
+                    },
+                    {
+                        key: 'upcoming',
+                        label: `Upcoming (0)`,
+                    },
+                    {
+                        key: 'overdue',
+                        label: `Overdue (0)`,
+                    },
+                    {
+                        key: 'no due date',
+                        label: `No due date (0)`,
+                    },
+                ]}
             />
             <Table
                 showHeader={false}
@@ -54,13 +113,18 @@ const TasksTable = () => {
                         <Empty
                             image="https://app.worklenz.com/assets/images/empty-box.webp"
                             imageStyle={{ height: 60 }}
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
                             description={
                                 <Typography.Text>
                                     No tasks to show.
                                 </Typography.Text>
                             }
                         />
-                    )
+                    ),
                 }}
             ></Table>
         </Card>
