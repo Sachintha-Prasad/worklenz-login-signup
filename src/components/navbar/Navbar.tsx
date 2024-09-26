5463825a86974a9cbbd5a0e765c4b71956e9c0c2
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-26 18:21:33 +0530

    refactor(navbar): create seperate component for profile button and switch team and add them

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,9 @@
 import React, { useState } from 'react'
 import logo from '../../assets/images/logo.png'
-import {
-    Avatar,
-    Button,
-    Col,
-    Drawer,
-    Dropdown,
-    Flex,
-    Menu,
-    Space,
-    Tooltip,
-    Typography,
-} from 'antd'
+import { Button, Col, Drawer, Flex, Menu, Tooltip, Typography } from 'antd'
 import { NavLink, useLocation } from 'react-router-dom'
 import {
-    BankOutlined,
-    BellOutlined,
-    CheckCircleTwoTone,
     ClockCircleOutlined,
-    DownOutlined,
     HomeOutlined,
     MenuOutlined,
     ProjectOutlined,
@@ -32,6 +17,8 @@ import { colors } from '../../styles/colors'
 import { useTranslation } from 'react-i18next'
 import NotificationButton from '../../features/notification/NotificationButton'
 import NotficationDrawer from '../../features/notification/NotficationDrawer'
+import SwitchTeamButton from '../../features/switchTeam/SwitchTeamButton'
+import ProfileButton from '../../features/userProfile/ProfileButton'
 
 const Navbar = () => {
     const location = useLocation()
@@ -56,32 +43,6 @@ const Navbar = () => {
         if (location.pathname.includes('/reporting')) return 'reporting'
         return 'path not matched'
     }
-
-    // Switch teams dropdown items
-    const items = [
-        {
-            key: '0',
-            label: (
-                <Space>
-                    <Avatar
-                        size="small"
-                        style={{ backgroundColor: colors.vibrantOrange }}
-                    >
-                        S
-                    </Avatar>
-                    <Flex vertical>
-                        <Typography.Text
-                            style={{ fontSize: 12, fontWeight: 300 }}
-                        >
-                            owned by you
-                        </Typography.Text>
-                        <Typography.Text>Sachintha Prasad</Typography.Text>
-                    </Flex>
-                    <CheckCircleTwoTone twoToneColor={colors.limeGreen} />
-                </Space>
-            ),
-        },
-    ]
 
     return (
         <Col
@@ -179,27 +140,11 @@ const Navbar = () => {
                                 {t('invite')}
                             </Button>
                         </Tooltip>
-                        <Tooltip title={t('switchTeamTooltip')}>
-                            <Dropdown menu={{ items }} trigger={['click']}>
-                                <Button
-                                    shape="round"
-                                    size="large"
-                                    icon={<BankOutlined />}
-                                    style={{
-                                        color: colors.skyBlue,
-                                        backgroundColor: colors.paleBlue,
-                                        border: 'none',
-                                        fontWeight: 500,
-                                    }}
-                                >
-                                    Sachintha Prasad
-                                    <DownOutlined />
-                                </Button>
-                            </Dropdown>
-                        </Tooltip>
-                        <Tooltip title={t('notificationTooltip')}>
-                            <NotificationButton />
-                        </Tooltip>
+
+                        <SwitchTeamButton />
+
+                        <NotificationButton />
+
                         <Tooltip title={t('help')}>
                             <Button
                                 shape="circle"
@@ -215,52 +160,17 @@ const Navbar = () => {
                                 }
                             />
                         </Tooltip>
-                        <Tooltip title={t('profileTooltip')}>
-                            <Button
-                                shape="circle"
-                                size="large"
-                                style={{
-                                    border: 'none',
-                                    boxShadow: 'none',
-                                }}
-                                children={
-                                    <UserOutlined style={{ fontSize: 20 }} />
-                                }
-                            />
-                        </Tooltip>
+
+                        <ProfileButton />
                     </Flex>
                 </Flex>
             )}
 
             {isTablet && !isDesktop && (
                 <Flex gap={8} align="center">
-                    <Dropdown menu={{ items }} trigger={['click']}>
-                        <Button
-                            shape="round"
-                            size="large"
-                            icon={<BankOutlined />}
-                            style={{
-                                color: colors.skyBlue,
-                                backgroundColor: colors.paleBlue,
-                                border: 'none',
-                                fontWeight: 500,
-                            }}
-                        >
-                            Sachintha Prasad
-                            <DownOutlined />
-                        </Button>
-                    </Dropdown>
-                    <Tooltip title={t('notificationTooltip')}>
-                        <NotificationButton />
-                    </Tooltip>
-                    <Tooltip title={t('profileTooltip')}>
-                        <Button
-                            shape="circle"
-                            size="large"
-                            style={{ border: 'none', boxShadow: 'none' }}
-                            children={<UserOutlined style={{ fontSize: 20 }} />}
-                        />
-                    </Tooltip>
+                    <SwitchTeamButton />
+                    <NotificationButton />
+                    <ProfileButton />
 
                     <Button
                         shape="circle"
@@ -274,17 +184,8 @@ const Navbar = () => {
 
             {isMobile && (
                 <Flex align="center">
-                    <Tooltip title={t('notificationTooltip')}>
-                        <NotificationButton />
-                    </Tooltip>
-                    <Tooltip title={t('profileTooltip')}>
-                        <Button
-                            shape="circle"
-                            size="large"
-                            style={{ border: 'none', boxShadow: 'none' }}
-                            children={<UserOutlined style={{ fontSize: 20 }} />}
-                        />
-                    </Tooltip>
+                    <NotificationButton />
+                    <ProfileButton />
 
                     <Button
                         shape="circle"
