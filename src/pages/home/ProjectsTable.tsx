0134dd3958087a632d3738a05973b49ceb36a94e
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-27 18:09:49 +0530

    refactor(project table): update project table when creating a project

--- src/pages/home/ProjectsTable.tsx
+++ src/pages/home/ProjectsTable.tsx
@@ -1,11 +1,20 @@
-import { SyncOutlined } from '@ant-design/icons'
-import { Button, Card, Empty, Flex, Segmented, Table, Typography } from 'antd'
+import { StarFilled, SyncOutlined } from '@ant-design/icons'
+import { Button, Card, Empty, Flex, Segmented, Tooltip, Typography } from 'antd'
 import React from 'react'
+import { useAppSelector } from '../../hooks/useAppSelector'
 
 const ProjectsTable = () => {
+    const projectsList = useAppSelector(
+        (state) => state.projectReducer.projects
+    )
+
     return (
         <Card
-            title="Projects (0)"
+            title={
+                <Typography.Title level={5} style={{ marginBlockEnd: 0 }}>
+                    Projects ({projectsList.length})
+                </Typography.Title>
+            }
             extra={
                 <Flex gap={8} align="center">
                     <Button shape="circle" icon={<SyncOutlined />} />
@@ -17,27 +26,38 @@ const ProjectsTable = () => {
             }
             style={{ width: '100%' }}
         >
-            <Table
-                showHeader={false}
-                locale={{
-                    emptyText: () => (
-                        <Empty
-                            image="https://app.worklenz.com/assets/images/empty-box.webp"
-                            imageStyle={{ height: 60 }}
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                            }}
-                            description={
-                                <Typography.Text>
-                                    You have not assigned to any project yet.
-                                </Typography.Text>
-                            }
-                        />
-                    ),
-                }}
-            ></Table>
+            {projectsList.length === 0 ? (
+                <Empty
+                    image="https://app.worklenz.com/assets/images/empty-box.webp"
+                    imageStyle={{ height: 60 }}
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                    description={
+                        <Typography.Text>
+                            You have not assigned to any project yet.
+                        </Typography.Text>
+                    }
+                />
+            ) : (
+                <div>
+                    {projectsList.map((project, index) => (
+                        <Flex key={index} gap={8} align="center">
+                            <Tooltip title={'Add to favourites'}>
+                                <StarFilled
+                                    style={{
+                                        fontSize: 20,
+                                        color: '#5f5f5f7f',
+                                    }}
+                                />
+                            </Tooltip>
+                            <Typography.Text>{project.name}</Typography.Text>
+                        </Flex>
+                    ))}
+                </div>
+            )}
         </Card>
     )
 }
